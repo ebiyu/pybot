8e08100c7703d59a277786ca8f66c2981d0a1a5c
海老原祐輔 <ebiyuu1121@gmail.com>
2018-06-17 21:24:39 +0900

    [add] APIエラー対応・ログ出力を追加

--- slack.py
+++ slack.py
@@ -1,3 +1,7 @@
+import mylogger
+from logging import getLogger
+logger=getLogger('slackLib')
+
 import requests ,json
 import key
 WEB_HOOK_URL=key.WEB_HOOK_URL()
@@ -20,28 +24,41 @@ api_headers={
 
 def sendWithAPI(text,to,name='',icon=''):
     url='https://slack.com/api/chat.postMessage'
-    requests.post(url, data = json.dumps({
+    resp=requests.post(url, data = json.dumps({
         'text': text,
         'username': name,
         'icon_emoji': icon,
         'channel' : to,
     }),headers = api_headers)
+    if resp.json()['ok']==False:
+        logger.error(resp.json()['error'])
+    else:
+        logger.info('sent "'+text+'" to '+to)
 
 def getChannelId(name):
     url='https://slack.com/api/channels.list'
     resp=requests.post(url,headers = api_headers)
-    channellist=(resp.json())['channels']
-    for i in range(len(channellist)):
-        if channellist[i]['name']==name:
-            return channellist[i]['id']
+    if resp.json()['ok']:
+        logger.debug('successfully got id for #'+name)
+        channellist=(resp.json())['channels']
+        for i in range(len(channellist)):
+            if channellist[i]['name']==name:
+                return channellist[i]['id']
+        logger.error('channel not found')
+    else:
+        logger.error(resp.json()['error'])
 
 def getChannelList():
     url='https://slack.com/api/channels.list'
     resp=requests.post(url,headers = api_headers)
-    channellist=(resp.json())['channels']
-    for i in range(len(channellist)):
-        channellist[i]=channellist[i]['name']
-    return channellist
+    if resp.json()['ok']:
+        logger.debug('successfully got channel list')
+        channellist=(resp.json())['channels']
+        for i in range(len(channellist)):
+            channellist[i]=channellist[i]['name']
+        return channellist
+    else:
+        logger.error(resp.json()['error'])
 
 def getUserList(channel):
     url='https://slack.com/api/channels.info'
@@ -49,7 +66,11 @@ def getUserList(channel):
         'token': API_TOKEN,
         'channel': getChannelId(channel)
     })
-    return resp.json()['channel']['members']
+    if resp.json()['ok']:
+        logger.debug('successfully got user list in #'+channel)
+        return resp.json()['channel']['members']
+    else:
+        logger.error(resp.json()['error'])
 
 def getUserRealName(userid):
     url='https://slack.com/api/users.info'
@@ -57,7 +78,11 @@ def getUserRealName(userid):
         'token': API_TOKEN,
         'user': userid
     })
-    return (resp.json())['user']['profile']['real_name']
+    if resp.json()['ok']:
+        logger.debug('successfully got user name of '+userid)
+        return (resp.json())['user']['profile']['real_name']
+    else:
+        logger.error(resp.json()['error'])
 
 def cleanChannel(channel,beforeHour):
     ts=(datetime.datetime.now()-datetime.timedelta(hours=beforeHour)).timestamp()
@@ -68,12 +93,23 @@ def cleanChannel(channel,beforeHour):
         'channel': chid,
         'latest': ts
     })
-    l=resp.json()['messages']
-    tslist=[]
-    delurl='https://slack.com/api/chat.delete'
-    for i in l:
-        tslist.append(i['ts'])
-        requests.post(delurl, data = json.dumps({
-            'channel' : chid,
-            'ts': i['ts']
-        }),headers = api_headers)
+    if resp.json()['ok']:
+        l=resp.json()['messages']
+        tslist=[]
+        delurl='https://slack.com/api/chat.delete'
+        for i in l:
+            tslist.append(i['ts'])
+            resp2=requests.post(delurl, data = json.dumps({
+                'channel' : chid,
+                'ts': i['ts']
+            }),headers = api_headers)
+            if resp.json()['ok']==False:
+                logger.error(resp.json()['error'])
+                break
+        else:
+            if len(l)!=0:
+                logger.info('successfully cleared '+str(len(l))+' message(s) in #'+channel)
+            else:
+                logger.debug('no message to delete in #'+channel)
+    else:
+        logger.error(resp.json()['error'])
